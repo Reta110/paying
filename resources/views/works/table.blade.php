85dd3f69842bf1a2b9de543c95ca347eb53cbe2d
Reta110 <rafaeltorrealba6@gmail.com>
2021-11-10 16:20:49 -0300

    table detail

--- resources/views/works/table.blade.php
+++ resources/views/works/table.blade.php
@@ -3,23 +3,28 @@
         <thead>
             <tr>
                 <th>User</th>
-        <th>Location</th>
-        <th>Activity</th>
-        <th>Amount</th>
-        <th>Quantity</th>
-        <th>Date</th>
+                <th>Location</th>
+                <th>Activity</th>
+                <th>Amount</th>
+                <th>Quantity</th>
+                <th>Date</th>
                 <th colspan="3">Action</th>
             </tr>
         </thead>
         <tbody>
-        @foreach($works as $work)
+            @foreach($works as $work)
             <tr>
                 <td>{{ $work->user->name }}</td>
-            <td>{{ $work->location->name }}</td>
-            <td>{{ $work->activity->name }}</td>
-            <td>{{ $work->activity->amount }}</td>
-            <td>{{ $work->quantity }}</td>
-            <td>{{ $work->date }}</td>
+                <td>{{ $work->location->name }}</td>
+                @if(isset($work->activity))
+                <td>{{ $work->activity->name }}</td>
+                <td>{{ $work->activity->amount }}</td>
+                @else
+                <td></td>
+                <td></td>
+                @endif
+                <td>{{ $work->quantity }}</td>
+                <td>{{ $work->date }}</td>
                 <td>
                     {!! Form::open(['route' => ['works.destroy', $work->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
@@ -30,7 +35,7 @@
                     {!! Form::close() !!}
                 </td>
             </tr>
-        @endforeach
+            @endforeach
         </tbody>
     </table>
     {{$works->links()}}
